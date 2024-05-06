4d2ff1a72ada25b1ce7ae88ee2c381ff2d31b970
Mohit <mohit.peak@gmail.com>
2024-05-06 19:09:32 +0530

    css fix

--- src/utils/MovieCarousel.tsx
+++ src/utils/MovieCarousel.tsx
@@ -1,6 +1,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import React from "react";
 import Slider from "react-slick";
 
 import MovieCard from "../components/MovieCard";
@@ -10,6 +11,49 @@ type MovieCarouselProps = {
   movies: MoviePosterProps[];
 };
 
+function Arrow(props) {
+  const { className, style, onClick } = props;
+  const arrowStyles = {
+    backgroundColor: "#999999",
+    height: "40px",
+    width: "40px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "50%",
+    opacity: 0.8,
+    cursor: "pointer",
+    zIndex: 999,
+    position: "absolute",
+    top: "50%",
+    right: props.type === "next" ? "50px" : undefined,
+    left: props.type === "prev" ? "10px" : undefined,
+  };
+
+  const arrowHoverStyles = {
+    backgroundColor: "#999988",
+    opacity: 1,
+  };
+
+  const [hover, setHover] = React.useState(false);
+
+  return (
+    <div
+      className={`${className} slick-arrow`}
+      style={{
+        ...style,
+        ...arrowStyles,
+        ...(hover ? arrowHoverStyles : {}),
+      }}
+      onClick={onClick}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {props.type === "next" ? ">" : "<"}
+    </div>
+  );
+}
+
 const MovieCarousel: React.FC<MovieCarouselProps> = ({ movies }) => {
   const settings = {
     dots: true,
@@ -18,6 +62,9 @@ const MovieCarousel: React.FC<MovieCarouselProps> = ({ movies }) => {
     slidesToScroll: 1,
     infinite: true,
     initialSlide: 0,
+    nextArrow: <Arrow type="next" />,
+    prevArrow: <Arrow type="prev" />,
+
     responsive: [
       {
         breakpoint: 1024,
@@ -47,20 +94,22 @@ const MovieCarousel: React.FC<MovieCarouselProps> = ({ movies }) => {
   };
 
   return (
-    <Slider {...settings}>
-      {movies.map((movie, index) => (
-        <div key={index}>
-          <MovieCard
-            movieImage={movie.poster}
-            name={movie.name}
-            genre={movie.genre}
-            rating={movie.rating}
-            voteCount={movie.voteCount}
-            id={movie.id}
-          />
-        </div>
-      ))}
-    </Slider>
+    <div className="slider-container">
+      <Slider {...settings}>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <MovieCard
+              movieImage={movie.poster}
+              name={movie.name}
+              genre={movie.genre}
+              rating={movie.rating}
+              voteCount={movie.voteCount}
+              id={movie.id}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
